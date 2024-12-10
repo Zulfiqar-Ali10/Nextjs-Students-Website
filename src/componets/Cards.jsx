b27be023676c76b016a8bff242b9cec90e5ab3df
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2024-12-10 08:08:26 +0500

    changes9

--- src/componets/Cards.jsx
+++ src/componets/Cards.jsx
@@ -12,10 +12,10 @@ export default function Cards() {
       <div className="sec1-2">
         <button className="btn1">all programme</button>
         <button>ui/ux design</button>
-        <button>program designs</button>
-        <button>program designs</button>
-        <button>program designs</button>
-        <button>program designs</button>
+        <button>program design</button>
+        <button>program design</button>
+        <button>program design</button>
+        <button>program design</button>
       </div>
       <div className="sec1-3">
         <div className="sec1-3-card1">
